c7c5002866f1ac52dcec4518f517a5ed079f9b56
adilaliviero-cpu <adila.liviero@gmail.com>
2026-02-08 16:38:36 +0100

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-// js/supabase.js
-const SUPABASE_URL = "https://nlgxfeemybrizoydzhw.supabase.co";
-const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZ3hmZWVtd3liaXV6b3ltZGh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NTI4OTAsImV4cCI6MjA4MDQyODg5MH0.-eldoV3CvMp0QmwuDxCFPjr4ztwJ1wZp4pB6ZP2TJJU";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
