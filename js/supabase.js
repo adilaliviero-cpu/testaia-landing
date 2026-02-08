909dbbccecc7dbda24340db4a86c78af24c41fdb
adilaliviero-cpu <adila.liviero@gmail.com>
2026-02-08 15:31:54 +0100

    Update Supabase anon key in supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // js/supabase.js
 const SUPABASE_URL = "https://SUA_URL_AQUI.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_CHAVE_PUBLICA_AQUI";
+const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZ3hmZWVtd3liaXV6b3ltZGh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NTI4OTAsImV4cCI6MjA4MDQyODg5MH0.-eldoV3CvMp0QmwuDxCFPjr4ztwJ1wZp4pB6ZP2TJJU";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
